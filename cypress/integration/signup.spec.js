68ac3a3044a7fd15bb3812959c082c57d4446d87
Wilson Alves Silva <wasyottatech@outlook.com>
2022-03-20 15:19:34 -0300

    Atualizando o Signup.spec

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -179,7 +179,7 @@ describe('Signup', () => {
             { field: 'postalcode', output: 'É necessário informar o CEP' },
             { field: 'number', output: 'É necessário informar o número do endereço' },
             { field: 'deliver_method', output: 'Selecione o método de entrega' },
-            { field: 'cnh', output: 'Adicione uma foto da sua CNH' }
+            { field: 'cnh', output: 'Adicione uma foto da sua CNH' },
         ]
 
         before(function () {
