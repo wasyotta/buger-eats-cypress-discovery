4d65f78bfce8e64da319e878a6843e26384d01ce
Wilson Alves Silva <wasyottatech@outlook.com>
2022-03-27 02:37:58 -0300

    Nova Alteração no arquivo index

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,8 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import '../support/commands'
-import '../support/cypress-file-upload'
+import 'commands'
+import 'cypress-file-upload'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
